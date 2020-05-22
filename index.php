4916e7ec148bfbf51f5f195d8cd3b4a044980355
Julia Evans <jevans8@mail.greenriver.edu>
2020-05-22 14:50:22 -0400

    Starter files installed F3 and routing

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+//require_once('model/data.php');
+
+//Start a session (AFTER the autoload)
+//session_start();
+
+//Instantiate the framework (Base class)
+$f3 = Base::instance(); //Class::method()
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//Default route
+$f3->route('GET /', function(){
+
+    echo '<h1>Test</h1>';
+    //$view = new Template();
+    //echo $view->render('views/home.html');
+
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//Run the framework (fat free)
+$f3->run();
+
